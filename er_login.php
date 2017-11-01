89987c974c55175e5273e85aee1f97250eb5fc91
Tarun Karthik <tarunkarthik999@gmail.com>
2017-11-01 07:28:44 +0530

    Create er_login.php

---
+++ er_login.php
@@ -0,0 +1,38 @@
+<?php
+$servername = "localhost";
+$username = "id2963861_wp_2be0db2d9519ee3205bea03d32c84df8";
+$password = "tarunkarthik";
+$dbname = "id2963861_wp_2be0db2d9519ee3205bea03d32c84df8";
+
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+if($_POST)
+{
+$uname = mysqli_real_escape_string($conn,$_POST['uname']);
+
+$psw = mysqli_real_escape_string($conn,$_POST['psw']);
+
+
+$sql = "SELECT * FROM `er_login` WHERE username = '$uname' AND password = '$psw'";
+$result = mysqli_query($conn,$sql);
+$count = mysqli_num_rows($result);
+
+
+if($count==1)
+{
+header("location: https://egovernence.000webhostapp.com/Elected-Representative-Page/");
+exit;
+}
+else
+{
+echo '<script type="text/javascript">alert("Login credential false");</script>';
+}
+}
+
+?>
